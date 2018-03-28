847b435ce05e8af00e5264b87e79e71802908b38
SamarV-121 <samarvispute121@gmail.com>
2018-03-28 10:02:14 +0530

    nicklaus: patches: update
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,10 @@
 cd system/core
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/0001-system_core.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/gatekeeperd-Add-use-software-gatekeeper-option.patch
 cd ../..
 cd hardware/interfaces
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/0002-hardware_interfaces.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/hidl-sensors-fallback-to-API_1_0.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/error-message-suppress-option.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/mtk-camera-blob.patch
 cd ../..
