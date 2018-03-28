faef4a4a9afa834ef6f8c87f88b292d4c1996cd2
SamarV-121 <samarvispute121@gmail.com>
2018-03-28 10:48:22 +0530

    nicklaus: patches: update and add patch for disabling vndk  for time being
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,11 @@
 cd system/core
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/gatekeeperd-Add-use-software-gatekeeper-option.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/Gatekeeperd-Add-use-software-gatekeeper-option.patch
 cd ../..
 cd hardware/interfaces
 git reset --hard && git clean -f -d
-patch -p1 < ../../device/motorola/nicklaus/patches/hidl-sensors-fallback-to-API_1_0.patch
-patch -p1 < ../../device/motorola/nicklaus/patches/error-message-suppress-option.patch
-patch -p1 < ../../device/motorola/nicklaus/patches/mtk-camera-blob.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/HIDL-sensors-fallback-to-API_1_0.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/Error-message-suppress-option.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/Workaround-mtk-camera-blob.patch
+patch -p1 < ../../device/motorola/nicklaus/patches/Disable-vndk-for-the-time-being.patch
 cd ../..
